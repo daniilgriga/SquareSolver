848aa13c9b6db8d66028363ef724abdc951d9810
daniilgriga <daniilextra13@gmail.com>
2024-08-21 21:57:13 +0300

    +structures

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -2,129 +2,244 @@
 #include <stdio.h>
 #include <math.h>
 
+#define TEST
+
 const int SS_INF_ROOTS = -1;
 
 const double EPS = 1e-9;
 
-void input(double* a, double* b, double* c);
-
-void output(int nRoots, double x1, double x2);
-
-int SquareSolver(const double a, const double b, const double c, double* x1, double* x2);
-
-int lin_equation(const double b, const double c, double* x);
-
-double fix_zero_sign(const double k);
-
-int Its_Zero(const double n);
-
-
-int main(void)
+struct equation
 {
-    // struct ...
-    // (*coeffs).a or coeffs -> a
+    double a;
+    double b;
+    double c;
+};
 
-    printf("# Solver of quadratic equations!\n");
-    printf("# Author: Daniil\n\n");
+struct quadratic
+{
+    double x1;
+    double x2;
+    int nRoots;
+};
 
-    double a = NAN;
-    double b = NAN;
-    double c = NAN;
+void input (equation* coeffs);
 
-    input(&a, &b, &c);
+void output (quadratic roots);
 
-    double x1 = 0, x2 = 0;
-    int nRoots = SquareSolver(a, b, c, &x1, &x2);
+void RunTests (void);
 
-    output(nRoots, x1, x2);
+int Its_Double_Equal (const double p, const double q);
+
+int RunTester (int nTests, const equation coeffs, double x1wanted, double x2wanted, int nRootsWanted);
+
+int SquareSolver (equation coeffs, quadratic* roots);
+
+int lin_equation (const equation coeffs, quadratic* roots);
+
+double fix_zero_sign (const double k);
+
+int Its_Zero (const double n);
+
+
+
+
+
+int main (void)
+{
+    struct equation coeffs = {.a = NAN,
+                              .b = NAN,
+                              .c = NAN};
+
+    struct quadratic roots = {.x1 = 0,
+                              .x2 = 0,
+                              .nRoots = 0};
+
+
+    printf ("# Solver of quadratic equations!\n");
+    printf ("# Author: Daniil\n\n");
+
+    //double a = NAN;
+    //double b = NAN;
+    //double c = NAN;
+
+#ifdef TEST
+    RunTests ();
+#endif
+
+
+    input (&coeffs);
+
+    //double x1 = 0;
+    //double x2 = 0;
+    //int nRoots = SquareSolver (coeffs, &x1, &x2);
+
+    output (roots);
 
     return 0;
 }
 
-int SquareSolver(const double a,const double b,const double c, double* x1, double* x2)
+int SquareSolver (equation coeffs, quadratic* roots)
 {
-    assert(x1 && "x1 is null");
-    assert(x2 && "x2 is null");
+    assert (roots != NULL); assert (( Its_Zero ((*roots).x1)) && "x1 is null");
+                            assert (( Its_Zero (roots->x2))&& "x2 is null");
 
-    assert(!isnan(a));
-    assert(!isnan(b));
-    assert(!isnan(c));
+    assert (!isnan (coeffs.a));
+    assert (!isnan (coeffs.b));
+    assert (!isnan (coeffs.c));
 
-    if(Its_Zero(a))
+    if (Its_Zero (coeffs.a))
     {
-        return lin_equation(b, c, x1);
+        return lin_equation (coeffs, roots);
     }
     else /* a != 0 */
     {
-        double d = ((b * b) - (4 * a * c));
+        double d = ((coeffs.b * coeffs.b) - (4 * coeffs.a * coeffs.c));
 
-        if(!Its_Zero(d) && d < 0)
-            return 0;
-        else
+        if (Its_Zero (d))
         {
-            *x1 = (-b + sqrt(d)) / (2 * a);
-            *x2 = (-b - sqrt(d)) / (2 * a);
+            roots->x1 = -coeffs.b / (2 * coeffs.a);
+            return 1;
+        }
+
+        else if (d > 0)
+        {
+            roots->x1 = (-coeffs.b - sqrt (d)) / (2 * coeffs.a);
+            roots->x2 = (-coeffs.b + sqrt (d)) / (2 * coeffs.a);
             return 2;
         }
+        else
+            return 0;
     }
 }
 
-int Its_Zero(const double n)
+int Its_Zero (const double n)
 {
-    return (fabs(n) < EPS);
+    return (fabs (n) < EPS);
 }
 
-int lin_equation(const double b,const double c, double* x)
+int lin_equation (equation coeffs, quadratic* roots)
 {
-    assert(x && "x is null");
+    assert (( Its_Zero ((*roots).x1)) && "x is null");
 
-    assert(!isnan(b));
-    assert(!isnan(c));
+    assert (!isnan (coeffs.b));
+    assert (!isnan (coeffs.c));
 
-    if(Its_Zero(b))
+    if (Its_Zero (coeffs.b))
     {
-        return ((Its_Zero(c)) ? SS_INF_ROOTS : 0);
+        return ((Its_Zero (coeffs.c)) ? SS_INF_ROOTS : 0);
     }
     else
     {
-        *x = -c / b;
+        roots->x1 = -coeffs.c / coeffs.b;
         return 1;
     }
 }
 
-double fix_zero_sign(const double k)
+double fix_zero_sign (const double k)
 {
-    return Its_Zero(k) ? 0 : k;
+    return Its_Zero (k) ? 0 : k;
 }
 
-void input(double* a, double* b, double* c)
+void input (equation* coeffs)
 {
-    printf("# Enter a, b, c: ");
-    scanf("%lf %lf %lf", a, b, c);
+    printf ("# Enter a, b, c: ");
+    scanf ("%lf %lf %lf", & (*coeffs).a, &coeffs->b, &coeffs->c);
 }
 
-void output(int nRoots, double x1, double x2)
+void output (quadratic roots)
 {
-    switch(nRoots)
+    switch (roots.nRoots)
     {
         case 0:
-            printf("No Roots!");
+            printf ("No Roots!");
             break;
+
         case 1:
-            printf("Single root: x = %lg", fix_zero_sign(x1));
+            printf ("Single root: x = %lg", fix_zero_sign (roots.x1));
             break;
+
         case 2:
-            if(fabs(x1 - x2) < EPS)
-            {
-                printf("x1 = x2 = %lg", fix_zero_sign(x1));
-                break;
-            }
-            else
-                printf("Two roots: x1 = %lg and x2 = %lg", fix_zero_sign(x1), fix_zero_sign(x2));
-                break;
-        case SS_INF_ROOTS:
-            printf("Infinite roots :)");
+            printf ("Two roots: x1 = %lg and x2 = %lg", fix_zero_sign (roots.x1), fix_zero_sign (roots.x2));
             break;
-        default: assert(0 && "Error");
+
+        case SS_INF_ROOTS:
+            printf ("Infinite roots :)");
+            break;
+
+        default: assert (0 && "Error1");
     }
+}
+
+int RunTester (int nTests, equation coeffs, double x1wanted, double x2wanted, int nRootsWanted)
+{
+    quadratic roots = {.x1 = 0,
+                       .x2 = 0,
+                       .nRoots = 0};
+
+    roots.nRoots = SquareSolver (coeffs, &roots);
+
+    if ((roots.nRoots) != nRootsWanted)
+    {
+        printf ("ERROR in %d Test\n", nTests);
+        return 1;
+    }
+
+    switch (roots.nRoots)
+    {
+        case 0:
+            return 1;
+
+        case 1:
+            if (!Its_Double_Equal (roots.x1, x1wanted))
+            {
+                printf ("ERROR Test %d: a = %lg, b = %lg, c = %lg, x1 = x2 = %lg, nRoots = %d\n"
+                        "Expected: x1 = x2 = %lg, nRoots = %d\n",
+                        nTests, coeffs.a, coeffs.b, coeffs.c, fix_zero_sign (roots.x1), roots.nRoots,
+                        x1wanted, nRootsWanted);
+                return 1;
+            }
+
+            break;
+
+        case 2:
+            if (!Its_Double_Equal (roots.x1, x1wanted) || !Its_Double_Equal (roots.x2, x2wanted))
+            {
+                printf ("ERROR Test %d: a = %lg, b = %lg, c = %lg, x1 = %lg, x2 = %lg, nRoots = %d\n"
+                       "Expected: x1 = %lg, x2 = %lg, nRoots = %d\n",
+                       nTests, coeffs.a, coeffs.b, coeffs.c, fix_zero_sign (roots.x1), fix_zero_sign (roots.x2), roots.nRoots,
+                       x1wanted, x2wanted, nRootsWanted);
+                return 1;
+            }
+
+            break;
+
+        case SS_INF_ROOTS:
+            return 1;
+
+        default: assert (0 && "Error");
+    }
+
+    return 0;
+}
+
+
+void RunTests (void)
+{
+    RunTester (1, {.a = 1, .b = 0, .c = -4}, -2, +2, 2);
+
+//                   a   b    c
+    RunTester ( 2, { 1, -2,   1},  1 ,      0, 1);
+    RunTester ( 3, { 4,  4,   1}, -0.5,     0, 1);
+    RunTester ( 4, { 1, -3, -10}, -2,       5, 2);
+    RunTester ( 5, { 0,  0,   0},  0,       0, SS_INF_ROOTS);
+    RunTester ( 6, {-1,  0,   0},  0,       0, 1);
+    RunTester ( 7, { 4,  8,   4}, -1,       0, 1);
+    RunTester ( 8, { 2,  4,   6},  0,       0, 0);
+    RunTester ( 9, { 1,  2,  -3}, -3,       1, 2);
+    RunTester (10, { 9, -6,   1},  1.0/3.0, 0, 1);
+}
+
+int Its_Double_Equal (const double p, const double q)
+{
+    return (fabs (p - q) < EPS);
 }
